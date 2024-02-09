81e21c327bd3a9eb47172071e6f1ce7292c0934f
Prince Bansal <prince.bansal008@gmail.com>
2024-02-09 14:04:25 +0530

    added server and initial end-points

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
 import express from "express";
 const app = express();
+const PORT = 3000;
 
-app.get("/", function (req, res) {
-    console.log("serving on PORT=3000");
+app.get("/", (req, res) => {
+    res.send("<h1>Hey... I'm Prince Bansal</h1>");
+});
+app.get("/about", (req, res) => {
+    res.send("<h1>About Me!</h1>");
+});
+app.get("/info", (req, res) => {
+    res.send("<h1>https://github.com/princebansal7</h1>");
 });
 
-app.listen(3000);
+app.listen(PORT, () => {
+    console.log(`listening on PORT=${PORT}`);
+});
