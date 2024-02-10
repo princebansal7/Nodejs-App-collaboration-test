15a156253eed17c0e944ee451b02ae166ab2194f
Prince Bansal <prince.bansal008@gmail.com>
2024-02-10 13:29:37 +0530

    Removed extra get end points

--- server.js
+++ server.js
@@ -5,12 +5,6 @@ const PORT = 3000;
 app.get("/", (req, res) => {
     res.send("<h1>Hey... I'm Prince Bansal</h1>");
 });
-app.get("/about", (req, res) => {
-    res.send("<h1>About Me!</h1>");
-});
-app.get("/contact", (req, res) => {
-    res.send("<h1>https://github.com/princebansal7</h1>");
-});
 
 app.listen(PORT, () => {
     console.log(`listening on PORT=${PORT}`);
